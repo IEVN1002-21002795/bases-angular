b385f9581e37c2948235cd1d981a07f5c754a84e
Diego Armando Hernandez Trujillo <diegotrujillo1899@gamil.com>
2024-09-21 10:15:14 -0600

    Commit sábado 21 de sep

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProductos } from '../productos';
  
 @Component({
   selector: 'app-product-list',
@@ -8,14 +9,24 @@ import { Component } from '@angular/core';
 export class ProductListComponent {
   
   //*ngIf=true, o false
+
+  imageWidht:number=60;
+  imageMargin:number=2;
+  muestraImg:boolean=true;
+  listFilter:string=''
+
+  muestImg():void{
+    this.muestraImg = !this.muestraImg;
+  }
+  //class="w-25"><img src="{{producto.ImagenUrl}}" alt="" class="w-50">
  
-  productos:any[]=[
+  productos:IProductos[]=[
     {
       "productoId":1,
       "Modelo":"Sentra",
       "Descripcion":"4 puertas",
       "Precio":2000,
-      "Year":2023,
+      "Year":"marzo 18 2023",
       "Marca":"NISSAN",
       "Color":"Azul",
       "ImagenUrl":"https://w7.pngwing.com/pngs/299/143/png-transparent-2018-nissan-sentra-car-2017-nissan-sentra-sv-vehicle-nissan-compact-car-sedan-car.png"
@@ -25,7 +36,7 @@ export class ProductListComponent {
       "Modelo":"Rio",
       "Descripcion":"4 puertas",
       "Precio":150000,
-      "Year":2020,
+      "Year":"septiembre 5 2014",
       "Marca":"KIA",
       "Color":"Azul",
       "ImagenUrl":"https://e7.pngegg.com/pngimages/149/836/png-clipart-kia-motors-car-2017-kia-rio-2005-kia-rio-kia-compact-car-sedan.png"
@@ -35,7 +46,7 @@ export class ProductListComponent {
       "Modelo":"Rio",
       "Descripcion":"4 puertas",
       "Precio":150000,
-      "Year":2020,
+      "Year":"april 12 2018",
       "Marca":"KIA",
       "Color":"Azul",
       "ImagenUrl":"https://e7.pngegg.com/pngimages/149/836/png-clipart-kia-motors-car-2017-kia-rio-2005-kia-rio-kia-compact-car-sedan.png"
---
+++ src/app/product/productos.ts
@@ -0,0 +1,11 @@
+export interface IProductos {
+    "productoId":number,
+    "Modelo":string,
+    "Descripcion":string,
+    "Precio":number,
+    "Year":string,
+    "Marca":string,
+    "Color":string,
+    "ImagenUrl":string
+}
+
